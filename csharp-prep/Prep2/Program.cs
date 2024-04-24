8209e8f9a2e5ed2bce2e33412bd2442f41398c57
Eli Hansen <15258789+emh68@users.noreply.github.com>
2024-04-24 15:58:41 -0600

    edits

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -8,40 +8,60 @@ class Program
         Console.Write("What is your grade percentage (i.e. 50, 80, etc.)? ");
         string gradePercentage = Console.ReadLine();
         int num = int.Parse(gradePercentage);
+        string letter = String.Empty;
+        string sign = String.Empty;
 
         if (num >= 90)
         {
-            Console.WriteLine("Your grade in the class is an A.");
+            letter = "A";
         }
 
         else if (num >= 80)
         {
-            Console.WriteLine("Your grade in the class is an B.");
+            letter = "B";
         }
 
         else if (num >= 70)
         {
-            Console.WriteLine("Your grade in the class is an C.");
+            letter = "C";
         }
 
         else if (num >= 60)
         {
-            Console.WriteLine("Your grade in the class is an D.");
+            letter = "D";
         }
 
         else
         {
-            Console.WriteLine("Your grade in the class is an F.");
+            letter = "F";
         }
+
+        if (num % 10 >= 7 && !(num >= 97 || num < 60))
+        {
+            sign = "+";
+        }
+
+        else if (num % 10 < 3 && !(num < 60))
+        {
+            sign = "-";
+        }
+
+        else
+        {
+            sign = "";
+        }
+
+        Console.WriteLine($"Your grade in the class is a {letter}{sign}. ");
+
 
         if (num >= 70)
         {
-            Console.Write("Congratulations, you passed the class!");
+            Console.Write("Congratulations, you passed the class! ");
         }
 
         else
         {
-            Console.Write("It's ok, you'll do better next time.");
+            Console.Write("It's ok, you'll do better next time. ");
         }
     }
 }
