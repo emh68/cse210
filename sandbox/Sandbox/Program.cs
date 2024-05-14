6075c6e477f1eddf83070de0174dd6c34ca7395b
Eli Hansen <15258789+emh68@users.noreply.github.com>
2024-05-14 15:32:54 -0600

    edits

--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -1,22 +1,54 @@
+
+
+
+// using System;
+// using System.IO;
+
+// class Program
+// {
+//     static void Main(string[] args)
+//     {
+
+//         string fileName = "myFile.txt";
+
+//         using (StreamWriter outputFile = new StreamWriter(fileName))
+//         {
+//             // You can add text to the file with the WriteLine method
+//             outputFile.WriteLine("This will be the first line in the file.");
+
+//             // You can use the $ and include variables just like with Console.WriteLine
+//             string color = "Blue";
+//             outputFile.WriteLine($"My favorite color is {color}");
+
+//         }
+//     }
+// }
+
+// using System;
+
+
+// class Program
+// {
+//     static void Main(string[] args)
+//     {
+//         Person p1 = new Person(); // Don't pass any information to get the default values
+//         Person p2 = new Person("Jane", "Doe"); // pass the first and last names
+//         Person p3 = new Person("Mrs.", "Jane", "Doe"); // pass all three variables
+
+//         Console.WriteLine($"{p3._title} {p3._firstName} {p3._lastName}");
+//         // Console.WriteLine(p2);
+//         // Console.WriteLine(p3);
+//     }
+// }
+
 using System;
-using System.IO;
+
 
 class Program
 {
     static void Main(string[] args)
     {
 
-        string fileName = "myFile.txt";
-
-        using (StreamWriter outputFile = new StreamWriter(fileName))
-        {
-            // You can add text to the file with the WriteLine method
-            outputFile.WriteLine("This will be the first line in the file.");
-
-            // You can use the $ and include variables just like with Console.WriteLine
-            string color = "Blue";
-            outputFile.WriteLine($"My favorite color is {color}");
-
-        }
     }
+
 }
