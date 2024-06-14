187c7117155872aca5bac4bac4efa23ad02409cb
Eli Hansen <15258789+emh68@users.noreply.github.com>
2024-06-14 13:49:03 -0600

    edited

--- sandbox/Sandbox/Person.cs
+++ sandbox/Sandbox/Person.cs
@@ -1,4 +1,3 @@
-
 // public class Person
 // {
 //     public string _title;
--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -53,13 +53,139 @@
 
 // }
 
-using System.Collections.Generic;
+// using System.Collections.Generic;
 
-List<string> words = new List<string> {"camera", "apple", "mouse", "keyboard"};
+// List<string> words = new List<string> {"camera", "apple", "mouse", "keyboard"};
 
-Console.WriteLine(words.Count);
+// Console.WriteLine(words.Count);
 
-for (int i = 0; i < words.Count; i++)
+// for (int i = 0; i < words.Count; i++)
+// {
+//     Console.WriteLine(words[i]);
+// }
+
+// using System;
+
+// public class Program
+// {
+//     static void Main()
+//     {
+//         Student student = new Student("Brigham", "234");
+//         string name = student.GetName();
+//         string number = student.GetNumber();
+//         Console.WriteLine(name);
+//         Console.WriteLine(number);
+//     }
+
+// }
+
+// public class Person
+// {
+//     private string _name;
+
+//     public Person(string name)
+//     {
+//         _name = name;
+//     }
+
+//     public string GetName()
+//     {
+//         return _name;
+//     }
+// }
+
+// public class Student : Person
+// {
+//     private string _number;
+
+//     public Student(string name, string number) : base(name)
+//     {
+//         _number = number;
+//     }
+
+//     public string GetNumber()
+//     {
+//         return _number;
+//     }
+
+//     public string GetStudentInfo()
+//     {
+//         return _name + " " + _number;
+//     }
+// }
+
+using System;
+
+namespace library_demo
 {
-    Console.WriteLine(words[i]);
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Book book1 = new Book();
+            book1.SetAuthor("Smith");
+            book1.SetTitle("A Great Book");
+
+            PictureBook book2 = new PictureBook();
+            book2.SetAuthor("Jones");
+            book2.SetTitle("Wonderful Picture Book");
+            book2.SetIllustrator("Mark Roberts");
+
+            Console.WriteLine(book1.GetBookInfo());
+            Console.WriteLine(book2.GetBookInfo());
+        }
+    }
+
+
+    public class Book
+    {
+        private string _author = "";
+        private string _title = "";
+
+        public string GetAuthor()
+        {
+            return _author;
+        }
+
+        public void SetAuthor(string author)
+        {
+            _author = author;
+        }
+
+        public string GetTitle()
+        {
+            return _title;
+        }
+
+        public void SetTitle(string title)
+        {
+            _title = title;
+        }
+
+        public virtual string GetBookInfo()
+        {
+            return $"{_title} by {_author}";
+        }
+    }
+    public class PictureBook : Book
+    {
+        private string _illustrator = "";
+
+        public string GetIllustrator()
+        {
+            return _illustrator;
+        }
+
+        public void SetIllustrator(string illustrator)
+        {
+            _illustrator = illustrator;
+        }
+        public override string GetBookInfo()
+        {
+            return $"{GetTitle()} by {GetAuthor()}, illustrated by {_illustrator}";
+        }
+    }
 }
+
+
+
