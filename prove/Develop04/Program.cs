5016f76739bc921fe141e5c78b03d60016440cfa
Eli Hansen <15258789+emh68@users.noreply.github.com>
2024-06-18 19:27:24 -0600

    Initial commit

--- prove/Develop04/Program.cs
+++ prove/Develop04/Program.cs
@@ -1,9 +1,35 @@
 using System;
+using System.IO;
+using System.Speech.Synthesis;
+
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Develop04 World!");
+
+        // SpeechSynthesizer synth = new SpeechSynthesizer();
+        // synth. SetOutputToDefaultAudioDevice();
+        // // synth.Speak("Hello Develop04 World!");
+
+        // synth.Rate = -1;
+
+        // synth.Speak("Take a deep breath in.");
+
+        // Thread.Sleep(5000);
+
+        // synth.Speak("Now slowly breathe out.");
+
+
+        // for (int i = 30; i > 0; i--)
+        // {
+        //     Console.Write(i);
+        //     Thread.Sleep(1000);
+        //     Console.Write("\b \b\b \b");
+        // }
+
+        // Console.WriteLine("Done.");
     }
 }
+
+
