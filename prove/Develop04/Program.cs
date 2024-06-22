72d7ca3a1bd8a05835ca1e15eb1c754a14add3ef
Eli Hansen <15258789+emh68@users.noreply.github.com>
2024-06-22 17:53:17 -0600

    edits

--- prove/Develop04/Program.cs
+++ prove/Develop04/Program.cs
@@ -9,8 +9,9 @@ class Program
     static void Main(string[] args)
     {
 
-        Activity activity = new Activity();
-        Console.WriteLine($"Welcome to the mindfulness program!");
+        // Activity activity = new Activity();
+        // Breathing breathing = new Breathing();
+        // Console.WriteLine($"Welcome to the mindfulness program!");
 
         bool quit = false;
 
@@ -26,39 +27,50 @@ class Program
             string choice = Console.ReadLine();
             if (choice == "1")
             {
-                Breathing.Run();
+                Breathing breathing = new Breathing("Breathing", "This activity will help you relax by walking you through a breathing exercise.\nYou will first breathe in, then you will hold that breath, then you will exhale through your mouth.\nClear your mind and focus on your breathing.", 0);
+                breathing.Run();
+            }
+
+            // else if (choice == "2")
+            // {
+            //     reflecting.Run();
+            // }
+
+            // else if (choice == "3")
+            // {
+            //     listing.Run();
+            // }
+
+            else if (choice == "4")
+            {
+                quit = true;
             }
         }
-
-
-
-        // Activity activity = new Activity("Breathing", "A simple breathing exercise", 30);
-        // activity.DisplayStartingMessage(activity.GetType().Name);
-        // activity.ShowCountDown(30);
-        // activity.DisplayEndingMessage();
-        // SpeechSynthesizer synth = new SpeechSynthesizer();
-        // synth.SetOutputToDefaultAudioDevice();
-        // synth.Speak("Hello Develop04 World!");
-
-        // synth.Rate = -1;
-
-        // synth.Speak("Take a deep breath in.");
-
-        // Thread.Sleep(5000);
-
-        // synth.Speak("Now slowly breathe out.");
-
-
-        // for (int i = 30; i > 0; i--)
-        // {
-        //     Console.Write(i);
-        //     Thread.Sleep(1000);
-        //     Console.Write("\b \b\b \b");
-        // }
-
-        // Console.WriteLine("Done.");
     }
-
 }
+// Activity activity = new Activity("Breathing", "A simple breathing exercise", 30);
+// activity.DisplayStartingMessage(activity.GetType().Name);
+// activity.ShowCountDown(30);
+// activity.DisplayEndingMessage();
+// SpeechSynthesizer synth = new SpeechSynthesizer();
+// synth.SetOutputToDefaultAudioDevice();
+// synth.Speak("Hello Develop04 World!");
 
+// synth.Rate = -1;
+
+// synth.Speak("Take a deep breath in.");
+
+// Thread.Sleep(5000);
+
+// synth.Speak("Now slowly breathe out.");
+
+
+// for (int i = 30; i > 0; i--)
+// {
+//     Console.Write(i);
+//     Thread.Sleep(1000);
+//     Console.Write("\b \b\b \b");
+// }
+
+// Console.WriteLine("Done.");
 
