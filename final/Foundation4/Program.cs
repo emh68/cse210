a65d801bbb49291def4b887104c03a3c0f2fe2cd
Eli Hansen <15258789+emh68@users.noreply.github.com>
2024-07-16 13:00:11 -0600

    added additional examples/data

--- final/Foundation4/Program.cs
+++ final/Foundation4/Program.cs
@@ -6,10 +6,16 @@ class Program
     static void Main(string[] args)
     {
         Running running = new Running(new DateTime(2024, 7, 15), 180, 20);
+        Running running1 = new Running(new DateTime(2024, 7, 16), 45, 5);
+        Running running2 = new Running(new DateTime(2024, 7, 17), 60, 8);
         Bike bike = new Bike(new DateTime(2024, 7, 15), 120, 10);
+        Bike bike1 = new Bike(new DateTime(2024, 7, 16), 90, 15);
+        Bike bike2 = new Bike(new DateTime(2024, 7, 17), 60, 12);
         Swimming swimming = new Swimming(new DateTime(2024, 7, 15), 60, 32);
+        Swimming swimming1 = new Swimming(new DateTime(2024, 7, 16), 45, 40);
+        Swimming swimming2 = new Swimming(new DateTime(2024, 7, 17), 30, 20);
 
-        Activity[] activities = { running, bike, swimming };
+        Activity[] activities = { running, running1, running2, bike, bike1, bike2, swimming, swimming1, swimming2 };
         foreach (var activity in activities)
         {
             Console.WriteLine(activity.GetSummary());
